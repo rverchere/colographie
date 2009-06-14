b595255d39fd6eb780cd877f69eade669b37be6a
rverchere <rverchere@fafd35fe-3e2d-11de-bd56-27b9897bf3d5>
2009-06-14 18:02:29 +0000

    ajout sauvegarde image - rudimentaire - sur GUI

--- colographieGui.py
+++ colographieGui.py
@@ -1,10 +1,51 @@
 #!/usr/bin/python
-# simple.py
- 
+# -*- coding:Utf-8 -*-
+
+# Import pour la GUI 
 import wx
- 
+# Import pour Gestion d'images
+import Image
+import ImageDraw
+
+# Fonction de chargement du fichier de configuration
+def loadConfig(filename):
+	fileCfg = open(filename, 'r')
+	global cfg
+	cfg = {} # On cree un dictionnaire vide
+	for line in fileCfg:
+		cle, x = line.split('\t', 1) # On isole la cle
+		x = x.rstrip() # On enleve les caracteres d espacement superflus a la fin
+		val = x.split('\t')[:3] # On separe les valeurs on ne garde que les 3 premieres
+		for i in range(len(val)): val[i]=int(val[i])
+		#print cle, val
+		cfg[cle] = val #On ajoute la ligne au dico
+	fileCfg.close()
+
+
+# Fonction de traduction du texte en code couleur
+def traductColor(text,squareSize,squareNumPerLine,squareSpace,imgName):
+	# image width = borders (squareSpace) + nb squares (size and space included)
+	imageWidth = squareSpace + ((int(squareSize)+squareSpace)*squareNumPerLine)
+	imageHeight = 300
+	squareX = squareY = squareSpace
+	im = Image.new("RGB", (imageWidth, imageHeight), 'white')
+	draw = ImageDraw.Draw(im)
+
+	# Create Squares
+	for i in range(len(text)):
+		draw.rectangle([(squareX,squareY), (squareX+squareSize,squareSize+squareY)], tuple(cfg[text.upper()[i]]))
+		squareX += squareSize+squareSpace
+		if squareX >= imageWidth:
+			squareY += squareSize+squareSpace
+			squareX = squareSpace
+
+	im.save(imgName, "PNG")
+
+
+		
 # Main Frame including all graphics
 class myFrame(wx.Frame):
+
     #__init__:begin
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title)
@@ -32,37 +73,46 @@ class myFrame(wx.Frame):
         hbox2 = wx.BoxSizer(wx.HORIZONTAL) # texte
         hbox3 = wx.BoxSizer(wx.HORIZONTAL) # afficher / sauvegarder
         
-        textSize = wx.StaticText(panel, -1, 'Taille: ')
-        spinSize = wx.SpinCtrl(panel, -1, '30', min=1)
-        textNb = wx.StaticText(panel, -1, 'Nombre: ')
-        spinNb = wx.SpinCtrl(panel, -1, '15', min=1)
-        hbox1.Add(textSize, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL)
-        hbox1.Add(textNb, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL)
-        hbox1.Add(spinSize, 1, wx.EXPAND)
-        hbox1.Add(spinNb, 1, wx.EXPAND)
+        self.textSize = wx.StaticText(panel, -1, 'Taille: ')
+        self.spinSize = wx.SpinCtrl(panel, -1, '30', min=1)
+        self.textNb = wx.StaticText(panel, -1, 'Nombre: ')
+        self.spinNb = wx.SpinCtrl(panel, -1, '15', min=1)
+        hbox1.Add(self.textSize, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL)
+        hbox1.Add(self.textNb, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL)
+        hbox1.Add(self.spinSize, 1, wx.EXPAND)
+        hbox1.Add(self.spinNb, 1, wx.EXPAND)
         vbox.Add(hbox1, 0, wx.EXPAND)
  
-        text = wx.TextCtrl(panel, wx.ID_ANY, style=wx.TE_MULTILINE)
- 
-        hbox2.Add(text, 1, wx.EXPAND)
+        self.text = wx.TextCtrl(panel, wx.ID_ANY, style=wx.TE_MULTILINE)
+
+        hbox2.Add(self.text, 1, wx.EXPAND)
  
         vbox.Add(hbox2, 1, wx.EXPAND)
- 
+
         btnShow = wx.Button(panel, -1, 'Afficher')
         btnSave = wx.Button(panel, -1, 'Sauvegarder')
+		
         hbox3.Add(btnShow, 1, wx.EXPAND)
         hbox3.Add(btnSave, 1, wx.EXPAND)
         vbox.Add(hbox3, 0, wx.EXPAND)
  
         panel.SetSizer(vbox)
- 
+
+        self.Bind(wx.EVT_BUTTON, self.printTxt, btnSave)
+
         self.Centre()
         self.Show(True)
     #__init__:end        
- 
+
+    def printTxt(self, event):
+        traductColor(self.text.GetValue(),self.spinSize.GetValue(),self.spinNb.GetValue(),2,'toto.png')
+
     def OnQuit(self, event):
         self.Close()
     
+
+# ici début du programme
+loadConfig('couleurs.cfg')
 app = wx.App()
 myFrame(None, -1, 'myApplication')
 app.MainLoop()
